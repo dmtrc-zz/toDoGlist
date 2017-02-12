1c508c44c2e66da9d71e7565cad0199073351c9c
dmtrc <elderlybill@gmail.com>
2017-02-12 22:36:04 +0300

    wip

--- toDo.js
+++ toDo.js
@@ -48,88 +48,21 @@ panel.appendChild(panelHeading);
 panel.appendChild(panelBody);
 app.appendChild(panel);
 
-
+var taskArr = [];
 
 window.onload = function(){
 
   button.onclick = function(){
-    
-    var taskArr = [];
+    if (input.value != 0){
+      taskArr.push({
+        'content' : input.value,
+        'completed' : false,
+        'removed' : false,
+        'editing' : false
+      });
 
-    taskArr.push({
-      'content' : input.value,
-      'completed' : false,
-      'removed' : false,
-      'editing' : false
-    });
-
-    input.value = "";
-
-    // BUILD LIST
-    for (var i = 0; i<taskArr.length; i++){
-      var listItem = document.createElement("li");
-      listItem.className = "task-item list-group-item";
-      listGroup.appendChild(listItem);
-
-      // task text field
-      var textField = document.createElement("span");
-      textField.className = "text-field";
-
-      // task control buttons
-      var editButton = document.createElement("span");
-      editButton.className = "edit-button item-ctrl";
-      editButton.innerHTML = "edit";
-      var completeButton = document.createElement("span");
-      completeButton.className = "complete-button item-ctrl";
-      completeButton.innerHTML = "complete";
-      var removeButton = document.createElement("span");
-      removeButton.className = "remove-button item-ctrl";
-      removeButton.innerHTML = "remove";
-
-      // task editing input
-      var editInput = document.createElement("input");
-      editInput.className = "editing-input";
-      editInput.value = taskArr[i].content;
-
-      listItem.appendChild(editInput);
-      listItem.appendChild(removeButton);
-      listItem.appendChild(completeButton);
-      listItem.appendChild(editButton);
-      listItem.appendChild(textField);
-      textField.appendChild(document.createTextNode( taskArr[i].content ));
-    }
-
-    //Mark task as completed
-    completeButton.onclick = function(){
-      if (listItem.className.indexOf("completed") == -1){
-        listItem.className += " completed";
-        this.innerHTML = "undo";
-      } else {
-        listItem.className = "task-item list-group-item";
-        this.innerHTML = "complete";
-      }
-    }
-
-    //Remove task
-    removeButton.onclick = function(){
-      if (listItem.className.indexOf("removed") == -1){
-        listItem.className += " removed";
-      } else {
-        listItem.className = "task-item list-group-item";
-      }
-    }
-
-    //Edit task
-    editButton.onclick = function(){
-      if (listItem.className.indexOf("editing") == -1){
-        listItem.className += " editing";
-        editInput.focus();
-        this.innerHTML = "save";
-      } else {
-        textField.innerHTML = editInput.value;
-        listItem.className = "task-item list-group-item";
-        this.innerHTML = "edit";
-      }
+      remboRender();
+      input.value = "";
     }
   }
 
@@ -137,99 +70,87 @@ window.onload = function(){
 
 
 
+function remboRender(){
+  listGroup.innerHTML = "";
+  taskArr.forEach(function(item){
+    
+    var listItem = document.createElement("li");
+    listItem.className = "task-item list-group-item";
+    listGroup.appendChild(listItem);
 
+    // task text field
+    var textField = document.createElement("span");
+    textField.className = "text-field";
 
+    // task control buttons
+    var editButton = document.createElement("span");
+    editButton.className = "edit-button item-ctrl";
+    editButton.innerHTML = "edit";
+    var completeButton = document.createElement("span");
+    completeButton.className = "complete-button item-ctrl";
+    completeButton.innerHTML = "complete";
+    var removeButton = document.createElement("span");
+    removeButton.className = "remove-button item-ctrl";
+    removeButton.innerHTML = "remove";
 
+    // task editing input
+    var editInput = document.createElement("input");
+    editInput.className = "editing-input";
+    editInput.value = item.content;
 
+    listItem.appendChild(editInput);
+    listItem.appendChild(removeButton);
+    listItem.appendChild(completeButton);
+    listItem.appendChild(editButton);
+    listItem.appendChild(textField);
+    textField.appendChild(document.createTextNode( item.content ));
 
+    //Mark task as completed
+    completeButton.onclick = function(){
+      if (item.completed == false){
+        item.completed = true;
+        listItem.className += " completed";
+        this.innerHTML = "undo";
+      } else {
+        item.completed = false;
+        listItem.className = "task-item list-group-item";
+        this.innerHTML = "complete";
+      }
+      console.log(taskArr);
+      // render function!!!!!!!
 
+    }
 
-// TODOGLIST WORKS AS FUCK
-// window.onload = function(){
+    //Remove task
+    removeButton.onclick = function(){
+      if (item.removed == false){
+        item.removed = true;
+        this.parentNode.remove();
+      } else {
+        item.removed = false;
+        listItem.className = "task-item list-group-item";
+      }
+      // render function!!!!!!!
+    }
 
-//   button.onclick = function(){
-
-//     //ADD NEW TASK
-//     if( input.value.length > 0 ){
-//       var listItem = document.createElement("li");
-//       listItem.className = "task-item list-group-item";
-//       listGroup.appendChild(listItem);
-
-//       // task text field
-//       var textField = document.createElement("span");
-//       textField.className = "text-field";
-
-//       // task control buttons
-//       var editButton = document.createElement("span");
-//       editButton.className = "edit-button item-ctrl";
-//       editButton.innerHTML = "edit";
-//       var completeButton = document.createElement("span");
-//       completeButton.className = "complete-button item-ctrl";
-//       completeButton.innerHTML = "complete";
-//       var removeButton = document.createElement("span");
-//       removeButton.className = "remove-button item-ctrl";
-//       removeButton.innerHTML = "remove";
-
-//       // task editing input
-//       var editInput = document.createElement("input");
-//       editInput.className = "editing-input";
-//       editInput.value = input.value;
-
-//       listItem.appendChild(editInput);
-//       listItem.appendChild(removeButton);
-//       listItem.appendChild(completeButton);
-//       listItem.appendChild(editButton);
-//       listItem.appendChild(textField);
-//       textField.appendChild(document.createTextNode(input.value));
-//       input.value = "";
-//     }
-
-//     //Mark task as completed
-//     completeButton.onclick = function(){
-//       if (listItem.className.indexOf("completed") == -1){
-//         listItem.className += " completed";
-//         this.innerHTML = "undo";
-//       } else {
-//         listItem.className = "task-item list-group-item";
-//         this.innerHTML = "complete";
-//       }
-//     }
-
-//     //Remove task
-//     removeButton.onclick = function(){
-//       if (listItem.className.indexOf("removed") == -1){
-//         listItem.className += " removed";
-//       } else {
-//         listItem.className = "task-item list-group-item";
-//       }
-//     }
-
-//     //Edit task
-//     editButton.onclick = function(){
-//       if (listItem.className.indexOf("editing") == -1){
-//         listItem.className += " editing";
-//         editInput.focus();
-//         this.innerHTML = "save";
-//       } else {
-//         textField.innerHTML = editInput.value;
-//         listItem.className = "task-item list-group-item";
-//         this.innerHTML = "edit";
-//       }
-//     }
-//   }
-
-// }
-
-
-
-
-
-
-
-
-
-
-
+    //Edit task
+    editButton.onclick = function(){
+      if (item.editing == false){
+        item.editing = true;
+        listItem.className += " editing";
+        editInput.focus();
+        this.innerHTML = "save";
+      } else {
+        item.editing = false;
+        item.content = editInput.value;
+        listItem.className = "task-item list-group-item";
+        this.innerHTML = "edit";
+        textField.innerHTML = item.content;
+      }
+      console.log(taskArr);
+    }
+  });
+};
 
 
 

46f1a02eb7037b67c73da2d5e14353bae1b21efd
dmtrc <elderlybill@gmail.com>
2017-02-16 11:57:51 +0300

    jst structure

--- toDo.js
+++ toDo.js
@@ -149,126 +149,30 @@ function remboRender(){
 
 // SOME SERIOUS OBJECTS HERE
 
-
 function ListContainer(){
   this.render = function(){
-    //CREATE ELEMENTS
-    var panel = document.createElement("div");
-    panel.id = "list-container";
-    panel.className = "panel panel-default";
-
-    var panelHeading = document.createElement("div");
-    panelHeading.className = "panel-heading";
-
-    var panelBody = document.createElement("div");
-    panelBody.className = "panel-body";
-
-    var panelHeadingTitle = document.createElement("h1");
-    panelHeadingTitle.innerHTML = "ToDo some shit";
-
-    var formGroup = document.createElement("div");
-    formGroup.className = "form-group";
-
-    var inputGroup = document.createElement("div");
-    inputGroup.className = "input-group";
-
-    var input = document.createElement("input");
-    input.className = "form-control";
-    input.id = "input";
-    input.setAttribute('placeholder', 'Please enter some shit here');
-
-    var inputGroupBtn = document.createElement("div");
-    inputGroupBtn.className = "input-group-btn";
-
-    var button = document.createElement("button");
-    button.className = "btn btn-success";
-    button.id = "addTask";
-    button.innerHTML = "Add this shit";
-
-    var listGroup = document.createElement("ul");
-    listGroup.className = "list-group";
-
-    panelHeading.appendChild(panelHeadingTitle);
-    panelBody.appendChild(formGroup);
-      formGroup.appendChild(inputGroup);
-        inputGroup.appendChild(input);
-        inputGroup.appendChild(inputGroupBtn);
-          inputGroupBtn.appendChild(button);
-    panelBody.appendChild(listGroup);
-
-    panel.appendChild(panelHeading);
-    panel.appendChild(panelBody);
-    app.appendChild(panel);
   }
 }
 
 function ListItem(){
-  this.content = content;
-  this.completed = completed;
-  this.removed = removed;
-  this.editing = editing;
+  var content;
+  var completed;
+  var removed;
+  var editing;
 
   this.render = function(){
-    var listItem = document.createElement("li");
-
-    // task text field
-    var textField = document.createElement("span");
-    textField.className = "text-field";
-
-    // task control buttons
-    var editButton = document.createElement("span");
-    editButton.className = "edit-button item-ctrl";
-    editButton.innerHTML = "edit";
-    var completeButton = document.createElement("span");
-    completeButton.className = "complete-button item-ctrl";
-    completeButton.innerHTML = "complete";
-    var removeButton = document.createElement("span");
-    removeButton.innerHTML = "remove";
-    removeButton.className = "remove-button item-ctrl";
-    
-    // task editing input
-    var editInput = document.createElement("input");
-    editInput.className = "editing-input";
-    editInput.value = item.content;
-
-    listItem.appendChild(editInput);
-    listItem.appendChild(removeButton);
-    listItem.appendChild(completeButton);
-    listItem.appendChild(editButton);
-    listItem.appendChild(textField);
-    textField.appendChild(document.createTextNode( item.content ));
-
-    if (!item.removed && !item.completed && !item.editing){
-      listItem.className = "task-item task-item list-group-item";
-    } else if (item.removed){
-      listItem.className = " removed task-item task-item list-group-item";
-    } else if (item.completed){
-      listItem.className = " completed task-item task-item list-group-item";
-      completeButton.innerHTML = "undo";
-    } else if (item.editing){
-      listItem.className = " editing task-item task-item list-group-item";
-      editButton.innerHTML = "save";
-      editInput.focus();
-    }
   }
 
   //Remove task
   this.remove = function(){
-    item.removed = !item.removed;
-    this.render();
   }
 
   //Mark task as completed
   this.complete = function(){
-    item.completed = !item.completed;
-    this.render();
   }
 
   //Edit task
   this.edit = function(){
-    item.editing = !item.editing;
-    item.content = editInput.value;
-    this.render();
   }
 }
 
